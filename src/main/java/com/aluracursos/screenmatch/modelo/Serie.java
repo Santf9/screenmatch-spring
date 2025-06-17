8e1c39683ad609e998ea9c95b775906676b8be89
Santiago Ferreira <santiandresfer@hotmail.com>
2025-06-17 15:05:35 -0300

    Creo Clase Serie para mapear datos del record DatosSerie mediante Categorías

--- src/main/java/com/aluracursos/screenmatch/modelo/Serie.java
+++ src/main/java/com/aluracursos/screenmatch/modelo/Serie.java
@@ -1,5 +1,7 @@
 package com.aluracursos.screenmatch.modelo;
 
+import java.util.OptionalDouble;
+
 public class Serie {
 
     private String titulo;
@@ -9,4 +11,84 @@ public class Serie {
     private Categoria genero;
     private String actores;
     private String sinopsis;
+
+    public Serie(DatosSerie datosSerie) {
+        this.titulo = datosSerie.titulo();
+        this.totalTemporadas = datosSerie.totalTemporadas();
+        this.evaluacion = OptionalDouble.of(Double.valueOf(datosSerie.evaluacion())).orElse(0); // Valor por defecto si la evaluación no es un número válido
+        this.poster = datosSerie.poster();
+        this.genero = Categoria.fromString(datosSerie.genero().split(",")[0].trim()); // Asumiendo que el género es una cadena separada por comas
+        this.actores = datosSerie.actores();
+        this.sinopsis = datosSerie.sinopsis();
+    }
+
+    @Override
+    public String toString() {
+        return
+                "titulo='" + titulo + '\'' +
+                ", totalTemporadas=" + totalTemporadas +
+                ", evaluacion=" + evaluacion +
+                ", poster='" + poster + '\'' +
+                ", genero=" + genero +
+                ", actores='" + actores + '\'' +
+                ", sinopsis='" + sinopsis + '\'';
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTotalTemporadas() {
+        return totalTemporadas;
+    }
+
+    public void setTotalTemporadas(Integer totalTemporadas) {
+        this.totalTemporadas = totalTemporadas;
+    }
+
+    public Double getEvaluacion() {
+        return evaluacion;
+    }
+
+    public void setEvaluacion(Double evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getActores() {
+        return actores;
+    }
+
+    public void setActores(String actores) {
+        this.actores = actores;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+
 }
